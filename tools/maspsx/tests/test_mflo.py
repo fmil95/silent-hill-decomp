2e98317614a57443ae010aaefdee0c63d06b3355
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-02-20 23:55:33 -0300

    Integrate work from forks by @MsKrypt1 and @fgsfdsfgs
    This commit integrates all of the additions that have been done since the previous commit on this repository by users @MsKrypt1 and @fgsfdsfgs on their separate forks. As it was not possible to get more contact info from them in order to solicit pull requests, a decision was made to manually add their contents into the upstream repository. The many forks were creating a rabbit hole in regards to properly following the decompilation status, and should this measure not be agreed upon by the aforementioned forkers, please leave a comment here to start discussing matters.
    
    Co-Authored-By: MsKrypt1 <19950257+mskrypt1@users.noreply.github.com>
    Co-Authored-By: fgsfds <2997482+fgsfdsfgs@users.noreply.github.com>

---
+++ tools/maspsx/tests/test_mflo.py
@@ -0,0 +1,196 @@
+import unittest
+
+from maspsx import MaspsxProcessor
+
+
+from .util import strip_comments
+
+
+class TestMflo(unittest.TestCase):
+
+    def test_mflo_disabled(self):
+        lines = [
+            "mult	$4,$2",
+            "mflo	$4",
+            "#nop",
+            "#nop",
+            "mult	$4,$5",
+        ]
+        expected_lines = [
+            "mult	$4,$2",
+            "mflo	$4",
+            "mult	$4,$5",
+        ]
+
+        mp = MaspsxProcessor(lines, nop_mflo_mfhi=False)
+        res = mp.process_lines()
+
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_mflo_li(self):
+        """
+        li with large value will turn into two instructions (lui+ori)
+        which means no nop is required
+        """
+        lines = [
+            "	mflo	$3",
+            "	li	$5,-2004318071			# 0x88888889",
+            "	mult	$3,$5        ",
+        ]
+        expected_lines = [
+            "mflo\t$3",
+            "li\t$5,-2004318071",
+            "mult\t$3,$5",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_mflo_li_hex(self):
+        """
+        li with large value will turn into two instructions (lui+ori)
+        which means no nop is required
+        """
+        lines = [
+            "	mflo	$3",
+            "	li	$5,0x2aaaaaab		# 715827883",
+            "	mult	$3,$5        ",
+        ]
+        expected_lines = [
+            "mflo\t$3",
+            "li\t$5,0x2aaaaaab",
+            "mult\t$3,$5",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_mflo_bgez_no_set_noreorder(self):
+        """
+        Standard behaviour is to take the next instruction and insert a nop *after* it
+        """
+        lines = [
+            "	mflo	$2",
+            "	#nop",
+            "	#nop",
+            "	bgez	$2,$L14",
+            "	mult	$17,$3",
+        ]
+        expected_lines = [
+            "mflo\t$2",
+            "bgez\t$2,$L14",
+            "nop",
+            "mult\t$17,$3",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_mflo_bgez_set_reorder(self):
+        """
+        Due to .set noreorder the nop should go BEFORE the bgez
+        """
+        lines = [
+            "	mflo	$2",
+            "	#nop",
+            "	#nop",
+            "	.set	noreorder",
+            "	.set	nomacro",
+            "	bgez	$2,$L14",
+            "	mult	$17,$3",
+        ]
+        expected_lines = [
+            "mflo\t$2",
+            "nop",
+            ".set\tnoreorder",
+            "bgez\t$2,$L14",
+            "mult\t$17,$3",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_expand_div_mflo_load_from_register(self):
+        """
+        If the div requires expansion, aspsx checks whether the following
+        instruction reads from the mflo/mfhi destination register.
+
+        BUG: https://github.com/mkst/maspsx/issues/105
+        """
+        lines = [
+            "div	$2,$2,$7",
+            "subu	$2,$10,$2",
+            "mult	$2,$8",
+        ]
+        expected_lines = [
+            "div\t$zero,$2,$7",
+            "mflo\t$2",
+            "nop",
+            "subu\t$2,$10,$2",
+            "mult\t$2,$8",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_expand_divu_mflo_load_from_register(self):
+        lines = [
+            "divu	$2,$2,$7",
+            "subu	$2,$10,$2",
+            "mult	$2,$8",
+        ]
+        expected_lines = [
+            "divu\t$zero,$2,$7",
+            "mflo\t$2",
+            "nop",
+            "subu\t$2,$10,$2",
+            "mult\t$2,$8",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_expand_div_mflo_no_load_from_register(self):
+        lines = [
+            "div	$2,$2,$7",
+            "subu	$2,$10,$3",
+            "mult	$2,$8",
+        ]
+        expected_lines = [
+            "div\t$zero,$2,$7",
+            "mflo\t$2",
+            "subu\t$2,$10,$3",
+            "nop",
+            "mult\t$2,$8",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
+
+    def test_mflo_rem(self):
+        lines = [
+            "mflo	$7",
+            "#nop",
+            "sll	$2,$3,12",
+            "rem	$7,$7,$2",
+        ]
+        expected_lines = [
+            "mflo\t$7",
+            "sll\t$2,$3,12",
+            "nop",
+            "div\t$zero,$7,$2",
+            "mfhi\t$7",
+        ]
+        mp = MaspsxProcessor(lines)
+        res = mp.process_lines()
+        clean_lines = strip_comments(res)
+        self.assertEqual(expected_lines, clean_lines)
